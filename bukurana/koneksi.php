b14641ad57678c5d5069adcac9eb1e023b408b2a
Rana Fatrika <ranafatrika@gmail.com>
2022-06-06 20:14:46 +0700

    isbn

---
+++ bukurana/koneksi.php
@@ -0,0 +1,14 @@
+<?php
+$dbserver = 'localhost';
+$dbname = 'kuliahwebrana';
+$dbuser = 'root';
+$dbpassword = '';
+$dsn = "mysql:host={$dbserver};dbname={$dbname}";
+
+$connection = null;
+try {
+    $connection = new PDO($dsn, $dbuser, $dbpassword);
+}catch (Exception $exception){
+    die("Terjadi erorr: ".$exception->getMessage());
+
+}
---
+++ vendor/api.php
@@ -0,0 +1,6 @@
+<?php
+$var1 = 100;
+$var2 = 23;
+$hasil = $var1 * $var2;
+
+echo "Hasil perkaliannya adalah ={$hasil}";
